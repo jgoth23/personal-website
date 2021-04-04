64148c9e29b8cd9e936111961dead05d331c2ee7
Johnny Goth <Jgoth23@gmail.com>
2021-04-04 18:32:30 -0500

    building education page

--- src/components/pages/Education.js
+++ src/components/pages/Education.js
@@ -1,14 +1,50 @@
 import React from 'react'
+import * as AiIcons from 'react-icons/ai';
+// import { Link } from 'react-router-dom';
 
 function Education() {
   return (
-    <div className="card">
+   
+      <div className="ed-card">
+      <div className="profile-main">
+        <h1 className="ed-name">Education</h1>
+        <h3 className="ed-position">prime digital academy</h3>
+        {/* <h4>Elk River, MN jgoth23@gmail.com</h4> */}
 
-    <div className="education">
-      <h1>Education</h1>
-    </div>
+        <p className="ed-body">
+        Full Stack developer
+        </p>
+      </div>
+      {/* <div className="home">
+        <div className="profile-sidebar"></div>
+        <img src="" alt=""></img>
+        <ul class="social-list">
+          <li className="social-item">
+            <a className="social-link" href="#"></a>
+            <Link to="#" className="menu-bars">
+              <AiIcons.AiFillGithub />
+            </Link>
+          </li>
+          <li className="social-item">
+            <a className="social-link" href="#"></a>
+            <Link to="#" className="menu-bars">
+              <AiIcons.AiFillLinkedin />
+            </Link>
+          </li>
+        </ul>
+      </div> */}
     </div>
   );
 }
+
+//     {/* /* <div className="education">
+//     <h1 className="ed-position">Education</h1>
+//     <div></div>
+//     <p className="ed-name">Prime Digital Academy</p>
+//       <h1></h1>
+//     </div>
+//     </div>
+//   );
+// } */ */}
 
 export default Education;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,32 +8,33 @@ function Home() {
       <div className="profile-main">
         <h1 className="profile-name">Johnny Goth</h1>
         <h3 className="profile-position">Software Engineer</h3>
+        {/* <h4>Elk River, MN jgoth23@gmail.com</h4> */}
+
         <p className="profile-body">
-          sdkmsdlkf;ksdf;lkdsjsljg;lskg;lkas ds.nds.m,nds.fnm
+          I am Johnny Goth, an enthusiastic, self-motivated, and confident
+          software developer residing in Elk River, MN. I started a rigorous coding program at Prime Digital Academy in
+          Minneapolis. I am now pursuing positions within companies here in the
+          Twin Cities to build my career as a software developer.
         </p>
       </div>
       <div className="home">
         <div className="profile-sidebar"></div>
         <img src="" alt=""></img>
-        <ul>
-          <li>
-            <a href="#"></a>
+        <ul class="social-list">
+          <li className="social-item">
+            <a className="social-link" href="#"></a>
             <Link to="#" className="menu-bars">
               <AiIcons.AiFillGithub />
             </Link>
           </li>
-          <li>
-            <a href="#"></a>
+          <li className="social-item">
+            <a className="social-link" href="#"></a>
             <Link to="#" className="menu-bars">
               <AiIcons.AiFillLinkedin />
             </Link>
           </li>
-          <li>
-            <a href="#"></a>
-          </li>
         </ul>
       </div>
-      
     </div>
   );
 }
