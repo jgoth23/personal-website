c54dce3004974b6748c8eec330211dc5c082cdfe
Johnny Goth <Jgoth23@gmail.com>
2021-04-05 00:46:56 -0500

    logos added

--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -1,5 +1,8 @@
 import React from 'react';
-
+import {SiHtml5} from 'react-icons/si'
+import {SiCss3} from 'react-icons/si'
+import {SiJavascript} from 'react-icons/si'
+import {SiReact} from 'react-icons/si'
 import {FaCheck} from 'react-icons/fa'
 
 function Skills() {
@@ -11,6 +14,7 @@ function Skills() {
         {/* <h4>Elk River, MN jgoth23@gmail.com</h4> */}
 
         <p className="ed-body">Programming languages & tools</p>
+        <div className="logo-list"> <SiHtml5 /> <SiCss3 /> <SiJavascript /> <SiReact /></div>
         
         <ul className="dates">
           <li className="checklist"><FaCheck /> html/css</li>
