4069f5cc16582dd0f35222b36a78183fef1736e5
Johnny Goth <Jgoth23@gmail.com>
2021-04-05 23:22:23 -0500

    styled more

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -6,15 +6,21 @@ function Projects() {
   return (
     <div className="ed-card">
       <div className="profile-main">
-        <h1 className="ed-name">Projects</h1>
-        <h3 className="ed-position">Coach's Tracker</h3>
-        
+        <h1 className="pro-name">Projects</h1>
+        <h3 className="pro-position">Coach's Tracker</h3>
 
-        <p className="ed-body">Full Stack developer</p>
-        <p className="dates">December 2020 - April 2021</p>
-        <h4 className="ed-position">Nature of the North</h4>
-        <p className="ed-body">graphic design</p>
-        <p className="dates">september 2009 - may 2010</p>
+        <p className="pro-body">FullStack(mobile/web)</p>
+        <p className="pro-body">Technologies I used</p>
+        <p className="pro-body">React, Redux</p>
+        <p className="pro-details">
+          Heres some info might change because the font color might be off but
+          we'll see what ends up being the result in the end just gonna try and
+          let it fill up then gp from there
+        </p>
+        <h4 className="pro-position">Nature of the North</h4>
+        <p className="pro-body">FullStack(mobile/web) </p>
+        <p className="pro-body">Technologies I used </p>
+        <p className="pro-details">september 2009 - may 2010</p>
       </div>
 
       {/* <div className="home">
